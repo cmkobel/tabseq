75fc1b7f31a8202e85fff06da8a17c036b7a1108
Carl M. Kobel <kobel@pm.me>
2021-06-01 23:23:58 +0200

    option to import basenames only, into the sample column, when using the read_fasta() function

--- R/tabseq.r
+++ R/tabseq.r
@@ -3,45 +3,52 @@
 #' @import dplyr stringr tidyr readr
 #' @description Takes the path to a fasta file. Loads the fasta file, and converts it to a tabseq table.
 #' @param file The path to a fasta-file
-#' @param from_fasta A boolean designating whether the file path points to a fasta-file
+#' @param basename_only If TRUE, the full paths to the files will be truncated in favor of the basename only
 #' @return A tibble containing the respective records and sequences from the fasta-file. Represented in the tabseq format.
 #' @examples
 #'
 #' #my_sequences = read_tabseq("path/to/sequences.fa")
 #'
-read_tabseq = function(file, from_fasta = T) {
+read_fasta = function(file, basename_only = T) {
 
+    # For spontaneous debugging:
+    # file = "~/assemblycomparator2/tests/E._faecium_plasmids/VB3240.fna"
 
-    if (from_fasta) {
-        # Open the file
-        cat(paste("reading", file, "as fasta", "\n"))
-        file_open = scan(file, what = "character", sep = NULL, quiet = T)
-
-
-        rv = dplyr::tibble(raw = file_open) %>%
-
-            # detect record headers
-            dplyr::mutate(header = if_else(stringr::str_sub(raw, 1, 1) == ">", T, F)) %>%
-
-            # enumerate the record headers and fill the downwards through the sequence lines
-            dplyr::mutate(header_num = ifelse(header, seq.int(nrow(.)), NA)) %>% # needs no sorting
-            tidyr::fill(header_num, .direction = "down") %>%
-
-            # Collect the lines for each record
-            dplyr::group_by(header_num) %>%
-            dplyr::summarize(part = stringr::str_sub(raw[1], 2),
-                             sequence = paste(raw[-1], collapse = ""), .groups = "drop") %>%
-
-            dplyr::mutate(comment = paste("record", dplyr::row_number(header_num))) %>%  # reset header nums
-
-            dplyr::transmute(sample = file, part, comment, sequence)
-
-        cat(paste("parsed", (rv %>% dim)[1], "records", "\n"))
-
-        rv
-
-
+    if (basename_only) {
+        file_presentable = basename(file)
+    } else {
+        file_presentable = file
     }
+
+    # Open the file
+    cat(paste("reading", file, "as fasta", "\n"))
+    file_open = scan(file, what = "character", sep = "\n", quiet = T)
+
+
+    rv = dplyr::tibble(raw = file_open) %>%
+
+        # detect record headers
+        dplyr::mutate(header = if_else(stringr::str_sub(raw, 1, 1) == ">", T, F)) %>%
+
+        # enumerate the record headers and fill the downwards through the sequence lines
+        dplyr::mutate(header_num = ifelse(header, seq.int(nrow(.)), NA)) %>% # needs no sorting
+        tidyr::fill(header_num, .direction = "down") %>%
+
+        # Collect the lines for each record
+        dplyr::group_by(header_num) %>%
+        dplyr::summarize(part = stringr::str_sub(raw[1], 2),
+                         sequence = paste(raw[-1], collapse = ""), .groups = "drop") %>%
+
+        dplyr::mutate(comment = paste("record", dplyr::row_number(header_num))) %>%  # reset header nums
+
+        dplyr::transmute(sample = file_presentable, part, comment, sequence)
+
+    cat(paste("parsed", (rv %>% dim)[1], "records", "\n"))
+
+    rv
+
+
+
 
 }
 
@@ -51,7 +58,7 @@ read_tabseq = function(file, from_fasta = T) {
 #' @description Takes a tabseq table, and saves it to disk in the fasta format.
 #' @param x The tabseq table to save
 #' @param file The path to a wanted fasta-file
-#' @param to_fasta A boolean designating whether the file should be written in the fasta format.
+#' @param format The format you want to save the file as
 #' @param record_format A string designating how to encode the records in the fasta file written. Defaults to "\%part", but may be a combination of all metadata columns including custom separators i.e. "\%sample|\%part|\%comment.
 #' @param verbose For debugging
 #' @return Saves the content to disk, and returns a tibble which represents what is being written to disk.
@@ -59,10 +66,9 @@ read_tabseq = function(file, from_fasta = T) {
 #'
 #' #my_sequences = read_tabseq("path/to/sequences.fa")
 #' #write_tabseq(my_sequences, "write/sequences/here/sequences.fa")
-write_tabseq = function(x, file, record_format = "%part", to_fasta = T, verbose = F) {
+write_fasta = function(x, file, format = "fasta", record_format = "%part", verbose = F) {
 
-    if (to_fasta) {
-
+    if (format == "fasta") {
 
         cat(paste("writing to file ", file, "as fasta", "\n"))
         cat(paste("using the following record_format:", record_format, "\n"))
